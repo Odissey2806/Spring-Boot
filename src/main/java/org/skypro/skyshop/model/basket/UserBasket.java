ebc88c0dc88bab91cebe240c1c4111a5e1d31404
Odissey2806 <aleksand2806@ya.ru>
2025-03-02 19:11:24 +0700

    commit Жизненный цикл компонентов Spring Boot

---
+++ src/main/java/org/skypro/skyshop/model/basket/UserBasket.java
@@ -0,0 +1,23 @@
+package org.skypro.skyshop.model.basket;
+
+import java.util.List;
+
+public class UserBasket {
+    private final List<BasketItem> basketItems;
+    private final int total;
+
+    public UserBasket(List<BasketItem> basketItems) {
+        this.basketItems = basketItems;
+        this.total = basketItems.stream()
+                .mapToInt(item -> item.getProduct().getPrice() * item.getQuantity())
+                .sum();
+    }
+
+    public List<BasketItem> getBasketItems() {
+        return basketItems;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+}
